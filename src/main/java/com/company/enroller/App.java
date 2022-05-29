066fac47a982e0f6c984162cae8357d06d4db70f
tomasz-nitychoruk <tafnitychoruk@gmail.com>
2022-05-29 11:05:56 +0200

    ex3

--- src/main/java/com/company/enroller/App.java
+++ src/main/java/com/company/enroller/App.java
@@ -3,6 +3,7 @@ package com.company.enroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class App {
 
